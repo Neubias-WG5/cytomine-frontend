fe2fb9e54606d7c56b3c0d527eb327a3825114de
Ulysse Rubens <ulyrubens@hotmail.com>
2018-09-18 15:22:01 +0200

    visibleNoTerm prop no more modified in ontology component

--- src/components/Explore/Ontology.vue
+++ src/components/Explore/Ontology.vue
@@ -9,7 +9,8 @@
 
         <div class="checkbox">
             <label>
-                <input v-model="visibleNoTerm" type="checkbox" name="showNoTermAnnotation" id="showNoTermAnnotation">
+                <input :checked="visibleNoTerm" @input="$emit('update:visibleNoTerm', !visibleNoTerm)"
+                       type="checkbox" name="showNoTermAnnotation" id="showNoTermAnnotation">
                 <label for="showNoTermAnnotation">Show annotations without terms</label>
             </label>
         </div>
