21a37dd83b83c8b1e3d100983e057653a0324be4
Ulysse Rubens <ulyrubens@hotmail.com>
2018-10-18 16:26:32 +0100

    Explorer and Software in same bundle

--- src/apps/explorer/main.js
+++ src/apps/explorer/main.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
-import Explorer from '../../components/Explore/Explorer.vue'
-import ImageGroupList from '../../components/ImageGroup/ImageGroupList'
 import Axios from 'axios';
-import * as uiv from 'uiv'
-import SlVueTree from 'sl-vue-tree'
-import 'vue-popperjs/dist/css/vue-popper.css';
-import AsyncComputed from 'vue-async-computed';
+
 import VueLayers from 'vuelayers';
 import ZoomifySource from './zoomify-source'
 import TranslateInteraction from './translate-interaction'
 import RotateInteraction from './rotate-interaction'
+
+import * as uiv from 'uiv'
+import SlVueTree from 'sl-vue-tree'
+import 'vue-popperjs/dist/css/vue-popper.css';
+import AsyncComputed from 'vue-async-computed';
+import Datatable from 'vue2-datatable-component'
+
+import Explorer from '../../components/Explore/Explorer.vue'
+import ImageGroupList from '../../components/ImageGroup/ImageGroupList'
+import Software from '../../components/Software/Software.vue'
 
 window.api = Axios.create({});
 
@@ -17,11 +22,12 @@ Vue.use(uiv);
 Vue.use(require('vue-moment'));
 Vue.use(SlVueTree);
 Vue.use(AsyncComputed);
+Vue.use(Datatable);
+
 Vue.use(VueLayers);
 Vue.use(ZoomifySource);
 Vue.use(TranslateInteraction);
 Vue.use(RotateInteraction);
-
 
 /* Multiple explore instances in old Backbone code base */
 window.exploreInstances = {};
@@ -44,4 +50,14 @@ window.setImageGroupTabInstance = function(id) {
         el:`#image-group-tab-${id}`,
         render: h => h(ImageGroupList)
     })
+};
+
+
+/* Software tab instance */
+window.softwareTabInstance = {};
+window.setSoftwareTabInstance = function () {
+    new Vue({
+        el: '#app-software',
+        render: h => h(Software)
+    });
 };
