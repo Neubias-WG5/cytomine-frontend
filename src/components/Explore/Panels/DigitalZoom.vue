f2306445230bfe15da2fa289dffd3011749f0ba8
Ulysse Rubens <ulyrubens@hotmail.com>
2018-11-21 11:31:17 +0100

    Default enabled digital zoom was not properly set.

--- src/components/Explore/Panels/DigitalZoom.vue
+++ src/components/Explore/Panels/DigitalZoom.vue
@@ -4,7 +4,7 @@
             <i class="fas fa-search"></i> Digital zoom
         </h4>
         <div class="alert alert-info">Enable or disable digital zoom (interpolation)</div>
-        <input @change="toggleDigitalZoom" v-model="digitalZoom" type="checkbox" :name="'digital-zoom-'+viewerId"
+        <input v-model="digitalZoom" type="checkbox" :name="'digital-zoom-'+viewerId"
                :id="'digital-zoom-'+viewerId">
         <label :for="'digital-zoom-'+viewerId">Digital zoom</label>
     </div>
@@ -18,14 +18,17 @@
         ],
         data() {
             return {
-                digitalZoom: true,
+                digitalZoom: false,
             }
         },
-        methods: {
-            toggleDigitalZoom() {
+        watch: {
+            digitalZoom() {
                 let increment = 4; // Max increment in project configuration ?
                 this.$emit('incrementMaxZoom', (this.digitalZoom) ? increment : (-1) * increment)
             }
+        },
+        mounted() {
+            this.digitalZoom = true;
         }
     }
 </script>
